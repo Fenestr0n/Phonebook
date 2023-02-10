44c0dece34e8e2936dd15098af230c4872a147b1
Roman Kozhin <kozhin.rm@gmail.com>
2023-02-10 18:18:08 +0300

    Add display_menu()

---
+++ view.py
@@ -0,0 +1,20 @@
+menu = '''Телефонный справочник:
+1 - Просмотр
+2 - Добавить
+3 - Изменить
+4 - Удалить
+5 - Импорт
+6 - Экспорт
+7 - Выход
+'''
+
+
+def display_menu():
+    while True:
+        print(menu)    
+        number = input(">>> ")
+        if number.isdigit():
+            if int(number) in (1, 2, 3, 4, 5, 6):
+                return number
+            elif int(number) == 7:
+                break
